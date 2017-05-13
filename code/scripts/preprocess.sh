ae4b67829d9aee02f91969174e08956880d96b24
Davide Pedranz <davide.pedranz@gmail.com>
2017-05-13 19:34:58 +0200

    Cleanup

--- code/scripts/preprocess.sh
+++ code/scripts/preprocess.sh
@@ -5,12 +5,9 @@
 #---------------------------------------------------------
 
 # make folder where to store the files
-this=$(dirname $0)
-data_raw="$this/../data_raw"
-data="$this/../data"
-mkdir -p $data
+mkdir -p "data"
 
 # merge features and concept tags togheter
 for type in "train" "test"; do
-	paste $data_raw/NLSPARQL.$type.feats.txt $data_raw/NLSPARQL.$type.data | cut -f 1,2,3,5 > $data/$type.txt
+	paste "data_raw/NLSPARQL.${type}.feats.txt" "data_raw/NLSPARQL.${type}.data" | cut -f 1,2,3,5 > "data/${type}.txt"
 done
