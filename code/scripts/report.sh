aa4fc8df6087581831aeca89697db143b97fcba9
Davide Pedranz <davide.pedranz@gmail.com>
2017-05-27 20:00:26 +0200

    [REPORT]: genetic algorithm

--- code/scripts/report.sh
+++ code/scripts/report.sh
@@ -4,21 +4,28 @@
 # to a LaTeX table to put in the report
 
 # parse the output of the evaluation script
+function parse_conlleval() {
+    sed -n 2p | sed 's/^.*precision: *\(.*\)%; recall: *\(.*\)%; FB1: *\(.*\)$/\1% \2% \3%/'
+}
 function parse_f1() {
-    cat - | sed -n 2p | sed 's/^.*precision: *\(.*\)%; recall: *\(.*\)%; FB1: *\(.*\)$/\1% \2% \3%/' | cut -d ' ' -f 3
+   parse_conlleval | cut -d ' ' -f 3
 }
 
 # format as a LaTeX table
 function latex() {
-    cat - | sed 's/ / \& /g' | sed 's/$/ \\\\/' | sed 's/_/\\_/g' | sed 's/%/\\%/g'
+    sed 's/ / \& /g' | sed 's/$/ \\\\/' | sed 's/_/\\_/g' | sed 's/%/\\%/g'
+}
+
+function parse_window_size_words() {
+    sed 's/\/performances$//'| sed 's/^.*words_window_\([0-9]\)_no_b/\1/'
 }
 
 function parse_window_size() {
-    cat - | sed 's/\/performances$//'| sed 's/^.*words_window_\([0-9]\)_no_b/\1/'
+    sed 's/\/performances$//'| sed 's/^.*window_\([0-9]\)/\1/'
 }
 
 function remove_new_line() {
-    cat - | tr '\n' ' '
+    tr '\n' ' '
 }
 
 this=$(dirname $0)
@@ -27,16 +34,39 @@ table="$this/../../report/table"
 
 mkdir -p $table
 
-# model with words only
 echo -n "----------------------------------------"
 echo -e "\n 1) model with words only"
 echo "----------------------------------------"
 echo "window, F1 (no bigrams), F1 (bigrams)"
 for f in $computations/basic/*_no_b/performances; do
-    window=$(echo $f | parse_window_size)
+    window=$(echo $f | parse_window_size_words)
     echo -n $window
     echo -n ' '
     cat $f | parse_f1 | remove_new_line
     cat `echo $f | sed 's/_no_b//'` | parse_f1
 done | tee >(latex > $table/01_words.tex)
+echo -e "----------------------------------------\n"
+
+echo -n "----------------------------------------"
+echo -e "\n 2) model with words + POS"
 echo "----------------------------------------"
+echo "window, prec, rec, F1"
+for f in $computations/intermediate/pos_*/performances; do
+    window=$(echo $f | parse_window_size)
+    echo -n $window
+    echo -n ' '
+    cat $f | parse_conlleval
+done | tee >(cut -d ' ' -f 1,4 | latex > $table/02_pos.tex)
+echo -e "----------------------------------------\n"
+
+echo -n "----------------------------------------"
+echo -e "\n 3) model with words + POS + stem"
+echo "----------------------------------------"
+echo "window, prec, rec, F1"
+for f in $computations/intermediate/stem_*/performances; do
+    window=$(echo $f | parse_window_size)
+    echo -n $window
+    echo -n ' '
+    cat $f | parse_conlleval
+done | tee >(cut -d ' ' -f 1,4 | latex > $table/03_stems.tex)
+echo -e "----------------------------------------\n"
